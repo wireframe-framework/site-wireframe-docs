56f63fa5e13e9380d3a3c2712ba75acb52299c09
teppokoivula <teppo.koivula@gmail.com>
2019-06-01 19:46:08 +0300

    Add hook method for replacing icon shorttags

--- ready.php
+++ ready.php
@@ -1,6 +1,22 @@
 <?php namespace ProcessWire;
 
 /**
+ * Replace [icon class="xxx] with <i class="xxx"></i>
+ *
+ * @param HookEvent $event
+ */
+function replaceIconShortcodes(HookEvent $event) {
+
+    // if found, replace icon shortags within rendered content
+    if (strpos($event->return, '[icon class="') !== false) {
+        $event->return = preg_replace('/\[icon class="([\-_a-z0-9 ]+)"\]/Ui', '<i class="$1"></i>', $event->return);
+    }
+}
+if ($page->template !== 'admin') {
+    $wire->addHookAfter('wireframe::render', 'replaceIconShortcodes');
+}
+
+/**
  * Minify HTML markup
  *
  * Concept itself is very simple, ie. getting event return value from
