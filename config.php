bff2d1388494f9be9a19fe9144903999f5d095a6
teppokoivula <teppo.koivula@gmail.com>
2019-06-23 00:36:38 +0300

    Add default overrides for MarkupMenu to site/config.php

--- config.php
+++ config.php
@@ -47,6 +47,18 @@ $config->debug = false;
  */
 $config->defaultAdminTheme = 'AdminThemeUikit';
 
+/**
+ * MarkupMenu default options
+ *
+ * @var array
+ *
+ */
+$config->MarkupMenu = [
+    'templates' => [
+        'nav' => '<nav class="{classes} menu--{menu_class_modifier}" aria-label="{aria_label}">%s</nav>',
+    ],
+];
+
 
 /*** INSTALLER CONFIG ********************************************************************/
 
