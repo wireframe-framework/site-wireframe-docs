d8340afdc3d2a996f6b988db0c661a170bedb603
teppokoivula <teppo.koivula@gmail.com>
2019-11-04 18:39:34 +0200

    Add JSON view for AJAX search

--- templates/controllers/SearchController.php
+++ templates/controllers/SearchController.php
@@ -8,7 +8,6 @@ class SearchController extends \Wireframe\Controller {
      * Init method
      */
     public function init() {
-
         // Hide the aside column
         $this->view->hide_aside = true;
     }
@@ -22,10 +21,21 @@ class SearchController extends \Wireframe\Controller {
 
         // Use SearchEngine for the site search feature.
         $searchEngine = $this->wire('modules')->get('SearchEngine');
-        return $searchEngine->render([
-            'form',
-            'results',
-        ]);
+
+        if ($this->wire('config')->ajax) {
+            // Respond AJAX query with JSON search results.
+            $this->view->setLayout(null)->setView('json');;
+            header('Content-Type: application/json');
+            return $searchEngine->renderResultsJSON();
+
+        } else {
+
+            // Render search form and results.
+            return $searchEngine->render([
+                'form',
+                'results',
+            ]);
+        }
     }
 
 }
